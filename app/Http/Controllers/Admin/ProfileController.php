6af058e2636589c87f2b9118f22f747b88ce7068
moriwakihikari <daiichidaimandaikichi.gi@gmail.com>
2020-01-23 16:14:23 +0000

    first commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -17,7 +17,7 @@ class ProfileController extends Controller
     }
     
     public function edit(){
-        return view('admin/profile/create');
+        return view('admin.profile.edit');
     
     }
     
